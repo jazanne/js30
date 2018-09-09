5808884293bdcc2c918194d04939834b63c54649
jazanne <bluelikejaz@gmail.com>
2018-09-09 12:24:14 -0400

    Plays sound that corrsponds to key pressed

--- drumkit/drumkit.js
+++ drumkit/drumkit.js
@@ -2,10 +2,20 @@ const PLAY_CLASS = 'playing';
 const SECOND = 1000;
 
 window.addEventListener('keydown', function(e){
+    // get key code
     const key = window.event ? e.keyCode : e.which;
-    const el = document.querySelector( `div[data-key="${key}"]` );
+
+    // form selector
+    const KEY_SELECTOR = `[data-key="${key}"]`
+
+    // get key "button"
+    const el = document.querySelector( `div${KEY_SELECTOR}` );
     el.classList.add( PLAY_CLASS );
     setTimeout(function(){
         el.classList.remove( PLAY_CLASS );
     }, SECOND / 10);
+
+    // get sound
+    const sound = document.querySelector( `audio${KEY_SELECTOR}`);
+    sound.play();
 });
