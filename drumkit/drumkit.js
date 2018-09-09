f4e00671d5cbc0316c4a64e20cdad25845e73b8f
jazanne <bluelikejaz@gmail.com>
2018-09-09 12:30:31 -0400

    Adds personal comments to how I can imporve project, based on 'official' solution from WB

--- drumkit/drumkit.js
+++ drumkit/drumkit.js
@@ -9,7 +9,7 @@ window.addEventListener('keydown', function(e){
     const KEY_SELECTOR = `[data-key="${key}"]`
 
     // get key "button"
-    const el = document.querySelector( `div${KEY_SELECTOR}` );
+    const el = document.querySelector( `div${KEY_SELECTOR}` );  
     el.classList.add( PLAY_CLASS );
     setTimeout(function(){
         el.classList.remove( PLAY_CLASS );
@@ -19,3 +19,15 @@ window.addEventListener('keydown', function(e){
     const sound = document.querySelector( `audio${KEY_SELECTOR}`);
     sound.play();
 });
+
+/*
+ *
+ * Notes to self, based on WesBos solution
+ * 
+ * 1. Remember that audio must be started from beginning
+ * 2. Handle cases where key pressed has no corresponding audio
+ * 3. Rely on "transitioned" event instead of timeouts
+ *    for knowing when transition has completed
+ * 4. Could have broken this into multiple functions
+ * 
+ */
